a1f77bcc40fa2b9123226729ac1f453f3fbab318
Adam Olgrzymek <mrasminaf@gmail.com>
2020-08-31 13:09:00 +0200

    Deleted id from UserForCreation

--- BlogPostsAPI/Models/UserForCreation.cs
+++ BlogPostsAPI/Models/UserForCreation.cs
@@ -2,7 +2,6 @@
 {
     public class UserForCreation
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string SecondName { get; set; }
         public int Age { get; set; }
