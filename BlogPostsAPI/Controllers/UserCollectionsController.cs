8334645740f75114e86d360bd0c843cc5f983125
Adam Olgrzymek <mrasminaf@gmail.com>
2020-09-02 14:49:19 +0200

    Added custom model binder for route ids

---
+++ BlogPostsAPI/Controllers/UserCollectionsController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BlogPostsAPI.Data;
+using BlogPostsAPI.Helpers;
+using BlogPostsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPostsAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserCollectionsController : ControllerBase
+    {
+        private readonly UserRepository userRepository;
+        private readonly IMapper mapper;
+
+        public UserCollectionsController(UserRepository userRepository,
+                IMapper mapper)
+        {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("({ids})")]
+        public ActionResult<IEnumerable<UserDTO>> GetUsers(
+            [FromRoute]
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<int> ids)
+        {
+
+        }
+    }
+}
---
+++ BlogPostsAPI/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BlogPostsAPI.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            if(!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            var name = bindingContext.ModelName;
+            var value = bindingContext.ValueProvider.GetValue(name).ToString();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+            }
+
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];
+            var converter = TypeDescriptor.GetConverter(elementType);
+
+            var values = value.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => converter.ConvertFromString(x.Trim()))
+                .ToArray();
+
+            var typedValues = Array.CreateInstance(elementType, values.Length);
+            values.CopyTo(typedValues, 0);
+            bindingContext.Model = typedValues;
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
