4628900583e2e2cffc1de355b92914a699059072
Adam Olgrzymek <mrasminaf@gmail.com>
2020-09-04 18:37:17 +0200

    Added basic classes for authorization

--- BlogPostsAPI/Data/BlogPostsDbContext.cs
+++ BlogPostsAPI/Data/BlogPostsDbContext.cs
@@ -1,15 +1,22 @@
-﻿using BlogPostsAPI.Entities;
+﻿using BlogPostsAPI.Authentication;
+using BlogPostsAPI.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogPostsAPI.Data
 {
-    public class BlogPostsDbContext : DbContext
+    public class BlogPostsDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<User> users { get; set; }
 
         public BlogPostsDbContext(DbContextOptions options) 
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
